2886e5527be6997b408ce6dfd58b334717aba89c
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2021-08-18 16:21:44 +0700

    Refactoring Tests: Задание №20 (group)

--- fixture/group.py
+++ fixture/group.py
@@ -64,6 +64,18 @@ class GroupHelper:
         wd.find_element_by_css_selector(f"input[value='{id}']").click()
         # wd.find_element_by_css_selector("input[value='%s']" % id).click()
 
+    def modify_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        # submit modification
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
@@ -84,7 +96,6 @@ class GroupHelper:
     def modify_by_name(self, group_name):
         wd = self.app.wd
         self.open_groups_page()
-        # selector_text = "input[title='Select ({0})']".format(group_name)
         selector_text = f"input[title='Select ({group_name})']"
 
         wd.find_element_by_css_selector(selector_text).click()
--- test/test_group_add.py
+++ test/test_group_add.py
@@ -3,16 +3,20 @@ __author__ = 'apavlenko'
 import pytest
 
 from model.group import Group
+
+
 # from data.groups import testdata
 
 
-def test_group_add(app, db, json_groups):
+def test_group_add(app, db, json_groups, check_ui):
     group = json_groups
     old_groups = db.get_group_list()
     app.group.create(group)
     new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
 
 # def test_group_add(app, json_groups):
 #     group = json_groups
