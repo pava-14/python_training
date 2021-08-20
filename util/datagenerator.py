09ecff2f8776433ddc6786dafb85d3c3ea6c026c
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2021-08-20 09:47:31 +0700

    Refactoring Tests: Задание №20,21,22 (CR)

--- util/datagenerator.py
+++ util/datagenerator.py
@@ -10,8 +10,8 @@ def get_random_string(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def get_randomx_string():
-    return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(random.randrange(20))])
+# def get_random_string_max20():
+#     return ''.join([random.choice(string.ascii_letters + string.digits) for i in range(random.randrange(20))])
 
 
 def random_string(prefix, maxlen):
