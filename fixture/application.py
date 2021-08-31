de54db9942488bc807642731e17a6391b1c6b186
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2021-08-31 17:14:07 +0700

    Задание №28: complete

--- fixture/application.py
+++ fixture/application.py
@@ -3,12 +3,25 @@ from selenium import webdriver
 from fixture.contact import ContactHelper
 from fixture.group import GroupHelper
 from fixture.session import SessionHelper
+from selenium.webdriver.chrome.options import Options
 
 
 class Application:
     def __init__(self, browser, base_url):
         if browser == "chrome":
-            self.wd = webdriver.Chrome()
+            options = Options()
+            # Runs Chrome in headless mode.
+            options.add_argument("--headless")
+            # Bypass OS security model
+            options.add_argument('--no-sandbox')
+            # applicable to windows os only
+            # options.add_argument('--disable-gpu')
+            options.add_argument('start-maximized')  #
+            options.add_argument('disable-infobars')
+            options.add_argument("--disable-extensions")
+            self.wd = webdriver.Chrome(chrome_options=options)
+            # driver = webdriver.Chrome(chrome_options=options,
+            #                           executable_path=r'C:\path\to\chromedriver.exe')
         elif browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "ie":
@@ -31,7 +44,8 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        if not (wd.current_url.endswith("/index.php") and len(wd.find_elements_by_id("search_count")) > 0):
+        if not (wd.current_url.endswith("/index.php")
+                and len(wd.find_elements_by_id("search_count")) > 0):
             wd.get(self.base_url)
 
     def return_to_home_page(self):
