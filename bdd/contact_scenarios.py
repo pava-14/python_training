d723c0979a1f51f4bdc7681a42351f4499d26999
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2021-08-31 18:00:11 +0700

    Задание №28: complete CR#1

--- bdd/contact_scenarios.py
+++ bdd/contact_scenarios.py
@@ -1,7 +1,7 @@
 __author__ = 'apavlenko'
 
 from pytest_bdd import scenario
-
+from .contact_steps import *
 
 @scenario('contacts.feature', 'Add new contact')
 def test_add_new_contact():
--- bdd/group_scenarios.py
+++ bdd/group_scenarios.py
@@ -2,6 +2,8 @@ __author__ = 'aapav'
 
 from pytest_bdd import scenario
 
+from .group_steps import *
+
 
 @scenario('groups.feature', 'Add new group')
 def test_add_new_group():
