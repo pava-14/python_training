81230a7b54a1d6b0e6b6a77b830bd6818cf3097f
Alexander Pavlenko <a.pavlenko@staffcop.ru>
2021-08-31 15:17:03 +0700

    Задание №27 complete CR#1

--- test/test_contact_add.py
+++ test/test_contact_add.py
@@ -3,36 +3,55 @@ __author__ = 'apavlenko'
 import pytest
 
 from model.contact import Contact
-from util.datagenerator import *
+from util.datagenerator import random_firstname, random_short_middlename
+from util.datagenerator import random_lastname, random_stret_address
+from util.datagenerator import random_email, random_phonenumber
 
 
 def test_add_contact(app, json_contacts, db, check_ui):
     old_contacts = db.get_contact_list()
     contacts = json_contacts
+
     app.contact.add_new_wo_group(contacts)
+
     new_contacts = db.get_contact_list()
     old_contacts.append(contacts)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    old_sorted = sorted(old_contacts, key=Contact.id_or_max)
+    new_sorted = sorted(new_contacts, key=Contact.id_or_max)
+
+    assert old_sorted == new_sorted
+
     if check_ui:
-        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list_from_home_page(),
-                                                                     key=Contact.id_or_max)
+        contacts_homepage = app.contact.get_contact_list_from_home_page()
+        old_sorted = sorted(contacts_homepage, key=Contact.id_or_max)
+        assert old_sorted == new_sorted
 
 
 def test_add_contact_wo_group(app, json_contacts):
     old_contacts = app.contact.get_contact_list_from_home_page()
     contacts = json_contacts
+
     app.contact.add_new_wo_group(contacts)
     new_contacts = app.contact.get_contact_list_from_home_page()
+
     assert len(old_contacts) + 1 == len(new_contacts)
+
     old_contacts.append(contacts)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    old_sorted = sorted(old_contacts, key=Contact.id_or_max)
+    new_sorted = sorted(new_contacts, key=Contact.id_or_max)
+
+    assert old_sorted == new_sorted
 
 
 testdata = [Contact(firstname="", middlename="", lastname="")] + [
-    Contact(firstname=random_firstname(), middlename=random_short_middlename(), lastname=random_lastname(),
-            address=random_stret_address(),
-            f_email=random_email(), s_email=random_email(), t_email=random_email(),
-            home_phone=random_phonenumber(), mobile_phone=random_phonenumber(), work_phone=random_phonenumber())
+    Contact(firstname=random_firstname(), middlename=random_short_middlename(),
+            lastname=random_lastname(), address=random_stret_address(),
+            f_email=random_email(), s_email=random_email(),
+            t_email=random_email(), home_phone=random_phonenumber(),
+            mobile_phone=random_phonenumber(),
+            work_phone=random_phonenumber())
     for i in range(3)
 ]
 
@@ -42,6 +61,12 @@ def test_add_contact_wo_group_annot(app, contact):
     old_contacts = app.contact.get_contact_list_from_home_page()
     app.contact.add_new_wo_group(contact)
     new_contacts = app.contact.get_contact_list_from_home_page()
+
     assert len(old_contacts) + 1 == len(new_contacts)
+
     old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    old_sorted = sorted(old_contacts, key=Contact.id_or_max)
+    new_sorted = sorted(new_contacts, key=Contact.id_or_max)
+
+    assert old_sorted == new_sorted
